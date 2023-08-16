da66ac6b94428bcbe22ed863761bb463c5e762a4
Charles Batiste <charles@batistelimited.com>
2023-08-16 16:00:13 +0200

    favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
-        <meta name="description" content="Kasizzle is a DJ and producer based in Stockholm, Sweden, with performances at Sturecompagniet, Hyde, Café Opera, and more."></meta>
+        <meta name="description" content="Kasizzle is a DJ and music producer based in Stockholm, Sweden, with performances at Sturecompagniet, Hyde, Café Opera, and more."></meta>
       </Head>
       <body>
         <Main />
