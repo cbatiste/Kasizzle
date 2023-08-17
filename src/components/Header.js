e4c3bf56819445b39c98a9a1106e850fd1381779
Charles Batiste <charles@batistelimited.com>
2023-08-17 13:08:54 +0200

    mobile header/navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
-import { FaApple, FaBars, FaInstagram, FaSpotify } from 'react-icons/fa';
+import { LiaBarsSolid, LiaTimesSolid } from 'react-icons/lia';
+import { FaApple, FaInstagram, FaSpotify } from 'react-icons/fa';
 import { BsEnvelope } from 'react-icons/bs';
 import { useEffect, useState } from 'react';
 
 function HeaderLink(props) {
   return (
-    <a className={'mx-3 cursor-pointer hover:text-zinc-400'} href={props.href}>
-      <h4 className={'header-light '}>{props.children}</h4>
+    <a className={'my-1 lg:my-0 mx-6 lg:mx-3 cursor-pointer hover:text-zinc-400'} href={props.href}
+       target={props.target}>
+      <h4 className={'text-xl lg:text-basis header-light'}
+          onClick={() => props.onClick && props.onClick()}>{props.children}</h4>
     </a>
   );
 }
 
 export default function Header() {
+  let [dropdownVisible, setDropdownVisible] = useState(false);
   let [headerOpaque, setHeaderOpaque] = useState(false);
+
+  const handleDropdownSelect = () => {
+    if (dropdownVisible) setDropdownVisible(false);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -31,10 +39,10 @@ export default function Header() {
   }, [headerOpaque]);
 
   return (
-    <header className={`fixed left-0 top-0 right-0 items-center py-3 px-5 lg:px-12 xl:px-[5%] z-10
-                        transition-all ${headerOpaque ? 'bg-[#0a0a0a]' : 'bg-[#0a0a0a] lg:bg-transparent'}`}>
+    <header className={`fixed left-0 top-0 right-0 items-center z-10 transition-all
+                        ${headerOpaque ? 'bg-[#0a0a0a]' : 'bg-[#0a0a0a] lg:bg-transparent'}`}>
       <div className={'flex flex-col'}>
-        <div className="flex w-full flex-row items-center">
+        <div className="flex w-full flex-row items-center py-3 px-5 lg:px-12 xl:px-[5%]">
           <div className={'flex-1 text-base hidden lg:flex flex-row'}>
             <HeaderLink href={'#performances'}>PERFORMANCES</HeaderLink>
             <HeaderLink href={'#about'}>ABOUT</HeaderLink>
@@ -48,8 +56,12 @@ export default function Header() {
             <img className="inline-block w-auto h-[40px] sm:h-[48px]" src="/logo-dark-sm.png" alt="Kasizzle Logo" />
           </div>
 
-          <div className={'flex grow text-2xl pb-1 justify-end lg:hidden'}>
-            <FaBars className={'cursor-pointer'} />
+          <div className={'flex grow text-2xl justify-end lg:hidden'}>
+            {dropdownVisible ? (
+              <LiaTimesSolid className={'cursor-pointer'} onClick={() => setDropdownVisible(false)} />
+            ) : (
+              <LiaBarsSolid className={'cursor-pointer'} onClick={() => setDropdownVisible(true)} />
+            )}
           </div>
 
           <div className={'hidden lg:flex flex-1 justify-end'}>
@@ -63,24 +75,33 @@ export default function Header() {
           </div>
         </div>
 
-        <div className={'flex flex-col hidden'}>
-          <div className={'flex flex-col'}>
-            <HeaderLink href={'#performances'}>PERFORMANCES</HeaderLink>
-            <HeaderLink href={'#about'}>ABOUT</HeaderLink>
-            <HeaderLink href={'#music'}>MUSIC</HeaderLink>
-            <div className={'w-full h-[1px] bg-neutral-400'}></div>
-            <HeaderLink href={'https://blog.kasizzle.se'}>BLOG</HeaderLink>
+        <div
+          className={`flex flex-col ${dropdownVisible ? 'border-t max-h-[220px] duration-500' : 'max-h-0 duration-300'}
+                      transition-all overflow-hidden lg:hidden border-neutral-900`}>
+          <div className={'flex flex-col mt-2'}>
+            <HeaderLink onClick={handleDropdownSelect} href={'#performances'}>PERFORMANCES</HeaderLink>
+            <HeaderLink onClick={handleDropdownSelect} href={'#about'}>ABOUT</HeaderLink>
+            <HeaderLink onClick={handleDropdownSelect} href={'#music'}>MUSIC</HeaderLink>
+            <HeaderLink onClick={handleDropdownSelect} href={'https://blog.kasizzle.se'}
+                        target={'_blank'}>BLOG</HeaderLink>
           </div>
 
-          <div className={'flex-row'}>
+          <div className={'flex flex-row p-4'}>
             <a href={'https://open.spotify.com/artist/5HAbkJVLDtTYmngkW16mrk'} target={'_blank'}
-               className={'mx-4 text-2xl'}><FaSpotify /></a>
+               onClick={handleDropdownSelect}
+               className={'mx-2 text-3xl lg:text-2xl'}><FaSpotify /></a>
             <a href={'https://music.apple.com/us/artist/kasizzle/1513360621'} target={'_blank'}
-               className={'mx-4 text-2xl'}><FaApple /></a>
+               onClick={handleDropdownSelect}
+               className={'mx-2 text-3xl lg:text-2xl'}><FaApple /></a>
             <a href={'https://www.instagram.com/kasizzle/'} target={'_blank'}
-               className={'mx-4 text-2xl'}><FaInstagram /></a>
-            <a href={'mailto:erik@kasizzle.se'} target={'_blank'} className={'mx-4 text-2xl'}><BsEnvelope /></a>
+               onClick={handleDropdownSelect}
+               className={'mx-2 text-3xl lg:text-2xl'}><FaInstagram /></a>
+            <a href={'mailto:erik@kasizzle.se'} target={'_blank'}
+               onClick={handleDropdownSelect}
+               className={'mx-2 text-3xl lg:text-2xl'}><BsEnvelope /></a>
           </div>
+
+          <div className={'w-full h-[1px] bg-neutral-900'}></div>
         </div>
       </div>
     </header>
